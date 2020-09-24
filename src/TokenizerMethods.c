9f08f7f8c5252ff3524642f92d5f3df8196505be
student <student@systems-vm.localdomain>
2020-09-24 16:36:04 -0600

    updated some methods

--- src/TokenizerMethods.c
+++ src/TokenizerMethods.c
@@ -22,24 +22,44 @@ int non_space_char(char c){
 }
 
 char *word_start(char *str){
-  char *p = str;
-  while(space_char(p)){
-    p++;
+  char *p= str;
+  char c = *p;
+  
+  // printf("%p\n",*p);
+  // printf("%p\n",*str);
+  while(space_char(c)){
+    *(p++);
+    c = *p;
   }
+  printf("%c\n",*p);
   return *p;
 }
 
 char *word_terminator(char *word){
-  char *p = word
+  char *p = word;
+  char c = *p;
   /* possibly use word count here?? */ 
-   while(non_space_char(p)){
-      p++;
-    }
-    return *p;
-    }
+  while(non_space_char(c)){
+     *(p++);
+     c = *p;
+  }
+  printf("%c\n",*p);
+  return *p;
+}
 
 int count_words(char *str){
   return 0;
 
   
   }
+char *copy_str(char *inStr, short len){
+  char *p = inStr;
+  return *p;
+}
+/*
+void print_tokens(char**){
+}
+void free_tokens(char**){
+}
+char **tokenize(char *s){
+  }*/
--- src/UserInterface.c
+++ src/UserInterface.c
@@ -1,13 +1,19 @@
 #include "tokenizer.h"
 #include <stdio.h>
-main()
+int main()
 {
   char str[50];
   printf("$");
   scanf("%[^\n]",str); /* [^\n] tells compilerr to read characters until \n is not found (until you find newline) */
   printf("%s\n",str);
-  int saver;
-  saver = non_space_char('p');
-  printf(saver);
+  int savedF = space_char(' ');
+  /*printf("%d\n", savedF);
+  savedF = space_char('\0');
+  printf("%d\n", savedF);
+  savedF = space_char('\t');
+  printf("%d\n", savedF);*/
+  char *str1 = "hap op";
+  word_start(str1);
+  // word_terminator(str1);
 }
   

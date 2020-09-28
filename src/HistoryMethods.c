f00c02877e05ba30285eb47898221960748c2b66
Laura-Blanco <lvblanco@miners.utep.edu>
2020-09-27 22:55:15 -0600

    finished history

--- src/HistoryMethods.c
+++ src/HistoryMethods.c
@@ -1,6 +1,7 @@
 #include "history.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "tokenizer.h"
 
 List *init_history()
 {
@@ -10,12 +11,52 @@ List *init_history()
 }
 void add_history(List *list, char *str)
 {
-  int id = 1;
+  int ident = 1;
   Item *current = list->root;
 
-  while(current->next != 0){
+  while(current->next != NULL){
     current = current->next;
-    id++;
+    ident++;
   }
   current->next = malloc(sizeof(Item));
+  char *start = word_start(str);
+  char *term = word_terminator(start);
+  current->next->str = copy_str(start, term-start);
+  current->next->id = ident;
+}
+
+char *get_history(List *list, int id)
+{
+  Item *current = list->root;
+
+  while(current != NULL){
+    if(current->id == id){
+      break;
+    }
+    current = current->next;
+  }
+  return current->str;
+}
+
+void print_history(List *list)
+{
+  Item *current = list->root->next;
+
+  while(current != NULL){
+    printf("%d. %s\n",current->id, current->str);
+    current = current->next;
+  }
+}
+
+void free_history(List *list)
+{
+  Item *current = list->root;
+  Item *temp;
+
+  while(current != NULL){
+    temp = current->next;
+    free(current);
+    current = temp;
+  }
+  free(list);
 }
--- src/UserInterface.c
+++ src/UserInterface.c
@@ -1,29 +1,36 @@
 #include "tokenizer.h"
 #include <string.h>
 #include <stdio.h>
+#include "history.h"
+
 int main()
 {
   char str[50];
   printf("$");
-  scanf("%[^\n]",str); /* [^\n] tells compilerr to read characters until \n is not found (until you find newline) */
+  scanf("%[^\n]",str); // [^\n] tells compilerr to read characters until \n is not found (until you find newline) 
   printf("%s\n",str);
   int savedF = space_char(' ');
-  /*printf("%d\n", savedF);
-  savedF = space_char('\0');
-  printf("%d\n", savedF);
-  savedF = space_char('\t');
-  printf("%d\n", savedF);*/
+  //printf("%d\n", savedF);
+ // savedF = space_char('\0');
+ // printf("%d\n", savedF);
+ // savedF = space_char('\t');
+ // printf("%d\n", savedF);
   char *str1 = " happy happy  joy  joy ";
   char **tokens = tokenize(str1);
-  printf("%s\n", tokens[0]);
-  printf("%s\n", tokens[1]);
-  printf("%s\n", tokens[2]);
-  printf("%s\n", tokens[3]);
+  print_tokens(tokens);
+  // printf("%s\n", tokens[0]);
+  // printf("%s\n", tokens[1]);
+  // printf("%s\n", tokens[2]);
+  // printf("%s\n", tokens[3]);
   // word_start(str1);
   // count_words(str1);
   // str1 = "happy  ";
   // count_words(str1);
   // copy_str(str1,5);
+  List *list = init_history();
+  add_history(list,"happy");
+  print_history(list);
+  add_history(list,"joyful");
+  print_history(list);
   
-}
-  
+  } 
